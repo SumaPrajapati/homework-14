4aabfef86acc2cc8060edcc25a93834fbd07f81e
Sumanlata Prajapati <sumanlataprajapati@Sumanlatas-MacBook-Pro.local>
2017-05-04 09:49:46 -0400

    Imperative & functional assignment14

--- hwkPractice.js
+++ hwkPractice.js
@@ -1,21 +1,45 @@
 
 
-/* Problem #2
-* Write a function that apply sales tax 10 % to the price of all item in the array. The function should return array of item.
-*/
+
 let reportcard = require('./reportcard');
 let items = require('./items');
 let animals = require('./animals');
 
 
+ /* Problem #1: Write a function that returns the subjects from a student's report card. Use the REPORTCARD
+  dataset as an input.*/
+ 
+  function subject(reportlist){
+      let sublist=[];
+      for(let i=0;i<reportlist.length; i++)
+      {
+         sublist.push({subject:reportlist[i].subject});
+      } 
+      return sublist; 
+}
+ function sub_subject(reportlist){
+     function s_func(reportitem){
+         return reportitem.subject;
+     }
+     return reportlist.map(s_func);
+ }
+console.log(subject(reportcard));
+console.log(sub_subject(reportcard));
+
+
+
+
+/* Problem #2
+* Write a function that apply sales tax 10 % to the price of all item in the array. The function should return array of item.
+*/
 function item_imperative(items){
     let saleTax=[];
     for(let i=0;i<items.length;i++){
-        let val=items[i].price * 1.2 // for tax 20%
-         
+        let taxval=items[i].price * 1.1 // for tax 10%
+        let total= items[i].price+taxval;
          saleTax.push({
              name:items[i].name,
-             price: val,
+             price: total,
          });
     }
     return saleTax;
@@ -23,8 +47,9 @@ function item_imperative(items){
 }
 function item_functional(items){
     function tax(item){
+
     return{ name:item.name,
-            price: item.price *  1.2 //for tax 20%
+            price:item.price + (item.price*  1.1 )//for tax 10%
        };
     }
 
@@ -38,22 +63,23 @@ console.log(items);
 *Problem #3  Write the function that return an array of the same size as an input array but filled with zeros. for 
 * example, the array [5,9,'hello'] would return [0,0,0]
 */
-let array=[5,9, 'hello']
-function arr_imperative(zeros){
-    let arr=[];
-    for(let i=0;i<zeros.length;i++){
-        arr.push(0);
+let arrlist=[5,9, 'hello'];
+function arr_imperative(items) {
+    let new_junk = [];
+    for (let i = 0; i < items.length; i++) {
+         new_junk.push(0);
     }
-   return arr;
-}
+ 
+    return new_junk;
+ }
 function arr_functional(zeros){
     function  arrZero(zero){
         return 0;
     }
    return zeros.map(arrZero);
 }
-console.log(arr_imperative(array));
-console.log(arr_functional(array));
+console.log(arr_imperative(arrlist));
+console.log(arr_functional(arrlist));
 
 /* 
 * Problem # 4   Write a function that return the abbreviation for provides phrase. The phraes should be consist of a capitalized version in the first
@@ -92,7 +118,7 @@ console.log(abb_functional(words));
 //step 2: check the each no its have negative so that no would be true 
 //step 3: check positive no would be false
 let arr=[5, 4,-7, -2, 0, -4, 8]
-function arr_imperative(arrray){
+function arr_imperative(array){
     let number=[];
    for(let i=0;i<array.length;i++){
        if(array[i]<0){
@@ -123,7 +149,7 @@ console.log(arr_functional(arr));
 * Problem # 5  Write the function that remove punctuation from the provided sentence(exclamation mark, periods, commas)
 */
 //let sentence=[];
-function p_imperative(word){
+function compare(word){
  let words=word.split('');
  let str =[];
  for(let i=0; i<words.length;i++){
@@ -135,7 +161,7 @@ function p_imperative(word){
   return str.join('');
 }
 
-function p_functional(word){
+function compare_func(word){
     function punch(strr){
         if(strr !=='!' && strr !=='.' && strr !== ','){
 
@@ -148,7 +174,43 @@ function p_functional(word){
     }
 
 //}
-console.log(p_imperative('U.S.A, is biggest country!')); 
-console.log(p_functional('U.S.A, is biggest country!'));
+console.log(compare('U.S.A, is a country!')); 
+console.log(compare_func('U.S.A, is a country!'));
+
+ /* Problem #7
+
+ * Write a function that accepts an array of farm animals and returns the distance of each animal from the barn
+ * (specified as a number). Use the ANIMALS array as an input for this problem, and assume the barn is at (0, 0).
+ */
+    function distance(animals){
+        let dist=[];
+       
+        let x=0;
+        let y=0;
+        
+        for(let i=0;i<animals.length; i++){
+            let x1=animals[i].x;
+            let y1=animals[i].y;
+           let total_dist=Math.sqrt((x1-x )*(x1-x)+ (y1-y)*(y1-y));
+           dist.push({nameofamimal:animals[i].species, distance:total_dist});
+
+        }
+        return dist;
+    } 
+    function animal_dist(animals){
+            function animal_func(animal){
+               let x=0;
+               let y=0;
+               let x1=animal.x;
+               let y1=animal.y;
+           let total_dist=Math.sqrt((x1-x )*(x1-x)+ (y1-y)*(y1-y));  
+            return ({nameofamimal:animal.species, distance:total_dist});
+        }
+        return animals.map(animal_func);
+    }
+ console.log(animal_dist(animals));
+ console.log(distance(animals));
+     
+     
 
 
